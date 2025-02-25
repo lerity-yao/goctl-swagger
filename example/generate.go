84a18805909bdb71aab9a83d4c6b5bd0d0855c1a
yaox <871966163@qq.com>
2025-02-25 16:42:53 +0800

    Forked Repository

---
+++ example/generate.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zeromicro/go-zero/tools/goctl/api/parser"
+	"github.com/zeromicro/go-zero/tools/goctl/plugin"
+	"github.com/zeromicro/goctl-swagger/generate"
+)
+
+func main() {
+	fileName := "swaggerTest.json"
+	host := "localhost:8080"
+	basepath := "/v1"
+	schemes := "https"
+
+	var plugin plugin.Plugin
+
+	var info struct {
+		ApiFilePath string
+		Style       string
+		Dir         string
+	}
+
+	info.ApiFilePath = "./example/user.api"
+	info.Style = "api"
+	info.Dir = "./"
+
+	plugin.ApiFilePath = info.ApiFilePath
+	plugin.Style = info.Style
+	plugin.Dir = info.Dir
+	api, err := parser.Parse(info.ApiFilePath)
+	plugin.Api = api
+
+	if err != nil {
+		fmt.Println(err, "create plugin")
+	}
+
+	err = generate.Do(fileName, host, basepath, schemes, &plugin)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
